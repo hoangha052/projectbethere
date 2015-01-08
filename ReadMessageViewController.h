415bfd3c6ae41d5d9487345c4470c8e28ff0509e
hoangha052 <hoangha052@gmail.com>
2015-01-08 16:58:13 +0700

    initial project

---
+++ ReadMessageViewController.h
@@ -0,0 +1,14 @@
+//
+//  ReadMessageViewController.h
+//  BeThere
+//
+//  Created by hoangha052 on 11/6/14.
+//  Copyright (c) 2014 hoangha052. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReadMessageViewController : UIViewController
+@property (nonatomic, strong) NSDictionary *message;
+
+@end
